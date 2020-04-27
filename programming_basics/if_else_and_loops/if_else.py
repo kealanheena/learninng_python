ffa4f4168cf5e775fc098371d7afed4d0818f0bc
kealanheena <kealanheena@gmail.com>
2020-04-27 23:21:50 +0100

    Chore: learn about python loops annd if/else statements

---
+++ programming_basics/if_else_and_loops/if_else.py
@@ -0,0 +1,11 @@
+check = "Yo"
+
+if check == False:
+    print("It's fasle")
+elif check == "Hamburger":
+    print("Yummmmm, hamburger")
+elif check == "Yo":
+    print("Hello")
+else:
+    print("It's true")
+
---
+++ programming_basics/if_else_and_loops/loops.py
@@ -0,0 +1,15 @@
+# FOR LOOP
+# names = ["Nick", "Jeff"]
+
+# for item in names:
+#     print("This persons name is", item)
+
+run = True
+current = 1
+
+while run:
+    if current == 100:
+        run = False
+    else:
+        print(current)
+        current += 1
