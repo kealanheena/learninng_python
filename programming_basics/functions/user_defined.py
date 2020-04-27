144e3383d4a2629997c1cc3252dc891ce855bff5
kealanheena <kealanheena@gmail.com>
2020-04-27 23:11:11 +0100

    Chore: learn different types of arguments

--- programming_basics/functions/user_defined.py
+++ programming_basics/functions/user_defined.py
@@ -13,4 +13,24 @@
 
 # print_something("Nick")
 
+# KEYWORD ARGUMENTS
+# def print_something(name = "Someone", age = "Unknown"):
+#     print("My name is ", name, "and my age is", age)
 
+# print_something(age = 27)
+
+# INFINATE ARGUMENTS
+# def print_people(*people):
+#     for person in  people:
+#         print("This person is", person)
+    
+# print_people("Nick", "Mark", "Jack", "Jon")
+
+# RETURN VALUES FROM FUNCTION
+def do_math(num1, num2):
+    return num1 + num2
+
+math1 = do_math(5, 7)
+math2 = do_math(11, 34)
+
+print("First sum is", math1, "and second sum is", math2)
