8a904a50c548b46146a5bdad79b47503140e20ec
kealanheena <kealanheena@gmail.com>
2020-04-27 23:00:09 +0100

    Chore: learn about python functions

---
+++ programming_basics/functions/built_in.py
@@ -0,0 +1,15 @@
+print("Hi")
+
+print(str(5))
+print(str(5.5))
+print(str(False))
+
+print(int("5"))
+print(float("5.5"))
+print(bool("True"))
+
+print(len("hello"))
+print(len(["hello", "bye"]))
+
+print(sorted([5, 1, 3, 10, 9]))
+print(sorted(['c', 'a', 'D', 'A', 'v', '1']))
---
+++ programming_basics/functions/user_defined.py
@@ -0,0 +1,16 @@
+
+# ADDING ARGUMENTS
+# def my_function(str1, str2):
+#     print(str1)
+#     print(str2)
+
+# my_function("Hello", "World")
+# my_function("Str one", "Str two")
+
+# DEFAULT ARGUMENTS
+# def print_something(name = "Someone", age = "Unknown"):
+#     print("My name is ", name, "and my age is", age)
+
+# print_something("Nick")
+
+
