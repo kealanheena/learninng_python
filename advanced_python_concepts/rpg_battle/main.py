533fbd9b9c87b31f9e58755345e3ef1d55adaef2
kealanheena <kealanheena@gmail.com>
2020-04-30 20:46:38 +0100

    Chore: add health bars top game

--- advanced_python_concepts/rpg_battle/main.py
+++ advanced_python_concepts/rpg_battle/main.py
@@ -2,6 +2,14 @@ from classes.game import Person, bcolors
 from classes.magic import Spell
 from classes.inventory import Item
 
+print("\n\n")
+print("NAME              HP                                  MP        ")
+print("                  _________________________           __________")
+print(bcolors.BOLD + "Valos:   " + 
+      "460/460 |" + bcolors.OKGREEN + "█████████████████████████" + bcolors.ENDC + bcolors.BOLD + "|   " + 
+      "65/65 |" + bcolors.OKBLUE + "██████████" + bcolors.ENDC +"|" + bcolors.ENDC)
+
+
 # Create Black Magic
 fire = Spell("Fire", 10, 100, "black")
 thunder = Spell("Thunder", 10, 100, "black")
